3d23275444e22174f70be55d6d6095a96d8513c4
Kensuke Nagae <kyanny@github.com>
2022-08-11 16:48:41 +0000

    Support JSON and TXT format as well as HTML

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'sinatra/json'
 require 'ipaddr'
+
+helpers do
+    def h(text)
+      Rack::Utils.escape_html(text)
+    end
+end
 
 get '/' do
     cidr = params['cidr'] || '192.168.0.0/24'
@@ -8,5 +15,16 @@ get '/' do
     rescue IPAddr::InvalidAddressError => error
         p error.inspect
     end
-    erb :index, locals: { ips: ips, cidr: cidr, error: error }
+
+    format = params['format'] || 'html'
+    p format
+    case format
+    when 'html'
+        erb :index, locals: { ips: ips, cidr: cidr, error: error }
+    when 'json'
+        json ips.to_a
+    when /te?xt/
+        content_type 'text/plain'
+        ips.map(&:to_s).join("\n") + "\n"
+    end
 end
